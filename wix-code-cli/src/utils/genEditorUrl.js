156a95a79bc1fa9d797d0bd062663d18f538c275
Leeor Aharon <leeor.aharon@gmail.com>
2019-03-25 14:29:30 +0200

    Added ssr exclusion and local mode experiment to editor URL

--- wix-code-cli/src/utils/genEditorUrl.js
+++ wix-code-cli/src/utils/genEditorUrl.js
@@ -1,7 +1,7 @@
 function genEditorUrl(useSsl, baseDomain, metasiteId, serverEditorPort) {
   return `${
     useSsl ? "https" : "http"
-  }://${baseDomain}/editor/${metasiteId}?localServerPort=${serverEditorPort}`;
+  }://${baseDomain}/editor/${metasiteId}?petri_ovr=specs.ExcludeSiteFromSsr=true&experiments=se_wixCodeLocalMode&localServerPort=${serverEditorPort}`;
 }
 
 module.exports = genEditorUrl;
