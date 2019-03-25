3681957a88231326843bb9ded530617d56865930
Leeor Aharon <leeor.aharon@gmail.com>
2019-03-25 14:25:17 +0200

    added a flag to pull and open-editor to ignore certificate errors

--- wix-code-cli/src/commands/open-editor.js
+++ wix-code-cli/src/commands/open-editor.js
@@ -1,17 +1,41 @@
 /* eslint-disable no-console */
+const process = require("process");
 const { app } = require("electron");
 const { openWindow, launch } = require("../utils/electron");
 const openEditorApp = require("../apps/open-editor");
 
 app &&
   app.on("ready", async () => {
+    if (process.env.IGNORE_CERTIFICATE_ERRORS) {
+      app.on(
+        "certificate-error",
+        (event, webContents, url, error, certificate, callback) => {
+          // On certificate error we disable default behaviour (stop loading the page)
+          // and we then say "it is all fine - true" to the callback
+          event.preventDefault();
+          callback(true);
+        }
+      );
+    }
+
     await openWindow(openEditorApp(), { show: true });
   });
 
 module.exports = {
   command: "open-editor",
   describe: "launches the local editor to edit the local site",
-  builder: args => args.option("C", { describe: "path", type: "string" }),
+  builder: args =>
+    args
+      .option("C", { describe: "path", type: "string" })
+      .option("ignore-certificate", {
+        describe: "ignore certificate errors",
+        type: "boolean"
+      }),
   handler: args =>
-    launch(__filename, { detached: true, stdio: "ignore", cwd: args.C })
+    launch(__filename, {
+      detached: true,
+      stdio: "ignore",
+      cwd: args.C,
+      env: { ...process.env, IGNORE_CERTIFICATE_ERRORS: "1" }
+    })
 };
--- wix-code-cli/src/commands/pull.js
+++ wix-code-cli/src/commands/pull.js
@@ -1,11 +1,25 @@
 /* eslint-disable no-console */
+const process = require("process");
 const { app } = require("electron");
 const { openWindow, launch } = require("../utils/electron");
 const pullApp = require("../apps/pull");
 
 app &&
   app.on("ready", async () => {
+    if (process.env.IGNORE_CERTIFICATE_ERRORS) {
+      app.on(
+        "certificate-error",
+        (event, webContents, url, error, certificate, callback) => {
+          // On certificate error we disable default behaviour (stop loading the page)
+          // and we then say "it is all fine - true" to the callback
+          event.preventDefault();
+          callback(true);
+        }
+      );
+    }
+
     await openWindow(pullApp(), { show: false });
+    app.exit(0);
   });
 
 module.exports = {
@@ -14,6 +28,10 @@ module.exports = {
   builder: args =>
     args
       .option("force", { describe: "force pull", type: "boolean" })
-      .option("C", { describe: "path", type: "string" }),
+      .option("C", { describe: "path", type: "string" })
+      .option("ignore-certificate", {
+        describe: "ignore certificate errors",
+        type: "boolean"
+      }),
   handler: args => launch(__filename, { cwd: args.C })
 };
