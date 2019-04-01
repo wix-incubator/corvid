0dea3d88a724b928af47f04e6182ce8414ef6d4c
Dor Itzhaki <dori@wix.com>
2019-04-01 14:54:22 +0300

    added corvid-debug binary

---
+++ wix-code-cli/src/cli.js
@@ -0,0 +1,9 @@
+const chalk = require("chalk");
+
+// eslint-disable-next-line no-console
+console.log(chalk.yellow("Corvid Technical Preview"));
+require("yargs")
+  .usage("Usage: $0 <command> [options]")
+  .commandDir("commands")
+  .help("help")
+  .demandCommand().argv;
--- wix-code-cli/src/index.js
+++ wix-code-cli/src/index.js
@@ -1,11 +1,3 @@
 #!/usr/bin/env node
 require("./monitoring/initSentry");
-const chalk = require("chalk");
-
-// eslint-disable-next-line no-console
-console.log(chalk.yellow("Corvid Technical Preview"));
-require("yargs")
-  .usage("Usage: $0 <command> [options]")
-  .commandDir("commands")
-  .help("help")
-  .demandCommand().argv;
+require("./cli");
