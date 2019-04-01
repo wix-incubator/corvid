1453d6a39ca8701c4778acbc0f0cda5bf4ba146b
Dor Itzhaki <dori@wix.com>
2019-04-01 19:18:54 +0300

    fixed local mode experiment name

--- corvid-cli/src/utils/genEditorUrl.js
+++ corvid-cli/src/utils/genEditorUrl.js
@@ -2,7 +2,7 @@ function genEditorUrl(useSsl, baseDomain, metasiteId, serverEditorPort) {
   const extraParams = process.env.QUERY ? `&${process.env.QUERY}` : "";
   return `${
     useSsl ? "https" : "http"
-  }://${baseDomain}/editor/${metasiteId}?petri_ovr=specs.ExcludeSiteFromSsr=true&experiments=se_corvidLocalMode&localServerPort=${serverEditorPort}${extraParams}`;
+  }://${baseDomain}/editor/${metasiteId}?petri_ovr=specs.ExcludeSiteFromSsr=true&experiments=se_wixCodeLocalMode&localServerPort=${serverEditorPort}${extraParams}`;
 }
 
 module.exports = genEditorUrl;
