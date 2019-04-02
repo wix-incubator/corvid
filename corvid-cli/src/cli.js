dc93c7e78cdc0452feb982003fbc4f8b16369153
Leeor Aharon <leeor.aharon@gmail.com>
2019-04-02 23:36:27 +0300

    Added another message to the cli's output

--- corvid-cli/src/cli.js
+++ corvid-cli/src/cli.js
@@ -2,6 +2,11 @@ const chalk = require("chalk");
 
 // eslint-disable-next-line no-console
 console.log(chalk.yellow("Corvid Technical Preview"));
+// eslint-disable-next-line no-console
+console.log(`
+Find out more about Corvid at www.wix.com/corvid
+The Corvid CLI is used for working with local versions of your Wix sites. Using the CLI you can create new local projects, open a local editor, and pull project updates.
+`);
 require("yargs")
   .usage("Usage: $0 <command> [options]")
   .commandDir("commands")
