f66b1aaad0e69ac94156eac5bfdf9ec5ca96fa5c
tombenezra <tombe@wix.com>
2019-04-04 19:25:48 +0300

    cli will run againts the prod viewer&editor

--- corvid-cli/src/utils/genEditorUrl.js
+++ corvid-cli/src/utils/genEditorUrl.js
@@ -2,7 +2,7 @@ function genEditorUrl(useSsl, baseDomain, metasiteId, serverEditorPort) {
   const extraParams = process.env.QUERY ? `&${process.env.QUERY}` : "";
   return `${
     useSsl ? "https" : "http"
-  }://${baseDomain}/editor/${metasiteId}?petri_ovr=specs.ExcludeSiteFromSsr=true&experiments=se_wixCodeLocalMode&EditorSource=1.6043.0&ReactSource=1.6971.0&localServerPort=${serverEditorPort}${extraParams}`;
+  }://${baseDomain}/editor/${metasiteId}?petri_ovr=specs.ExcludeSiteFromSsr=true&experiments=se_wixCodeLocalMode&localServerPort=${serverEditorPort}${extraParams}`;
 }
 
 module.exports = genEditorUrl;
