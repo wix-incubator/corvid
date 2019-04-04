246989804672c2673375a77d33f3939817d50932
Leeor Aharon <leeor.aharon@gmail.com>
2019-04-04 13:50:26 +0300

    warn/enforce minimal node version
    The cli will enforce version 10 or higher, and installing the local
    server will warn when the node version is not compatible.

--- corvid-cli/src/cli.js
+++ corvid-cli/src/cli.js
@@ -1,4 +1,5 @@
 const chalk = require("chalk");
+const version = require("./version");
 
 // eslint-disable-next-line no-console
 console.log(chalk.yellow("Corvid Technical Preview"));
@@ -7,9 +8,17 @@ console.log(`
 Find out more about Corvid at www.wix.com/corvid
 The Corvid CLI is used for working with local versions of your Wix sites. Using the CLI you can create new local projects, open a local editor, and pull project updates.
 `);
-require("yargs")
-  .usage("Usage: $0 <command> [options]")
-  .commandDir("commands")
-  .help("help")
-  .strict()
-  .demandCommand().argv;
+
+if (version.check()) {
+  require("yargs")
+    .usage("Usage: $0 <command> [options]")
+    .commandDir("commands")
+    .help("help")
+    .strict()
+    .demandCommand().argv;
+} else {
+  // eslint-disable-next-line no-console
+  console.log(
+    chalk.red("Unsupported Node.js version, please use", version.required)
+  );
+}
---
+++ corvid-cli/src/version.js
@@ -0,0 +1,12 @@
+"use strict";
+
+module.exports = {
+  required: "version 10 or higher",
+  check: () => {
+    const nodeVersion = process.versions.node;
+    // eslint-disable-next-line no-unused-vars
+    const [major, minor, patch] = nodeVersion.split(".");
+
+    return major >= 10;
+  }
+};
