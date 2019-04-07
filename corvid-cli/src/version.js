c47c26e62beff935bf86540a2d8f11bf8c3b521e
Leeor Aharon <leeor.aharon@gmail.com>
2019-04-08 00:24:14 +0300

    Increased the minimal required node version to 10.10.0
    This is due to the use of the fs.Dirent class in local-site

--- corvid-cli/src/version.js
+++ corvid-cli/src/version.js
@@ -7,6 +7,6 @@ module.exports = {
     // eslint-disable-next-line no-unused-vars
     const [major, minor, patch] = nodeVersion.split(".");
 
-    return major >= 10;
+    return (major === 10 && minor >= 10) || major > 10;
   }
 };
