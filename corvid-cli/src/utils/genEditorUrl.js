c8c761edd1bee1819b64ee9f103f20ec7a21fa0e
Leeor Aharon <leeor.aharon@gmail.com>
2019-04-07 11:04:10 +0300

    added corvidSessionId to the editor URL

--- corvid-cli/src/utils/genEditorUrl.js
+++ corvid-cli/src/utils/genEditorUrl.js
@@ -2,7 +2,9 @@ function genEditorUrl(useSsl, baseDomain, metasiteId, serverEditorPort) {
   const extraParams = process.env.QUERY ? `&${process.env.QUERY}` : "";
   return `${
     useSsl ? "https" : "http"
-  }://${baseDomain}/editor/${metasiteId}?petri_ovr=specs.ExcludeSiteFromSsr=true&experiments=se_wixCodeLocalMode&localServerPort=${serverEditorPort}${extraParams}`;
+  }://${baseDomain}/editor/${metasiteId}?petri_ovr=specs.ExcludeSiteFromSsr=true&experiments=se_wixCodeLocalMode&corvidSessionId=${
+    process.env.CORVID_SESSION_ID
+  }&localServerPort=${serverEditorPort}${extraParams}`;
 }
 
 module.exports = genEditorUrl;
