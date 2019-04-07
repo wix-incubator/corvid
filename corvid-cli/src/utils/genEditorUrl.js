8132a1f553c9fe48545eb77def8b48eb8b785b23
Dor Itzhaki <dori@wix.com>
2019-04-08 01:36:00 +0300

    remove auto enable of se_wixCodeLocalMode experiment via the editor's url

--- corvid-cli/src/utils/genEditorUrl.js
+++ corvid-cli/src/utils/genEditorUrl.js
@@ -2,7 +2,7 @@ function genEditorUrl(useSsl, baseDomain, metasiteId, serverEditorPort) {
   const extraParams = process.env.QUERY ? `&${process.env.QUERY}` : "";
   return `${
     useSsl ? "https" : "http"
-  }://${baseDomain}/editor/${metasiteId}?petri_ovr=specs.ExcludeSiteFromSsr=true&experiments=se_wixCodeLocalMode&corvidSessionId=${
+  }://${baseDomain}/editor/${metasiteId}?petri_ovr=specs.ExcludeSiteFromSsr=true&corvidSessionId=${
     process.env.CORVID_SESSION_ID
   }&localServerPort=${serverEditorPort}${extraParams}`;
 }
