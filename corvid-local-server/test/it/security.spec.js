7771bd4e19eeb98cfd0561e936a0e4ff0d1b4db5
Kosta Gershberg <konstanting@wix.com>
2019-04-23 16:56:03 +0300

    origin test

--- corvid-local-server/test/it/security.spec.js
+++ corvid-local-server/test/it/security.spec.js
@@ -1,8 +1,19 @@
 const { socketClient } = require("corvid-local-test-utils");
-const { localServer, closeAll } = require("../utils/autoClosing");
+const {
+  localServer,
+  closeAll,
+  editor: loadEditor
+} = require("../utils/autoClosing");
+const { siteCreators: sc } = require("corvid-local-test-utils");
+const { editorSiteBuilder } = require("corvid-fake-local-mode-editor");
+const { localSiteBuilder } = require("corvid-local-site/testkit");
 const { initLocalSiteWithConfig } = require("../utils/localSiteDir");
 
 const getEditorEndpoint = server => `http://localhost:${server.port}`;
+
+const getConfig = expectedLocalSite => ({
+  ".corvidrc.json": expectedLocalSite[".corvidrc.json"]
+});
 
 afterEach(closeAll);
 
@@ -110,5 +121,19 @@ describe("Security", () => {
         done();
       }
     );
+  });
+  it("should not permit requests with wrong origin", async () => {
+    process.env.NODE_ENV = "test-origin";
+    const siteItems = sc.fullSiteItems();
+
+    const editorSite = editorSiteBuilder.buildPartial(...siteItems);
+    const expectedLocalSite = localSiteBuilder.buildPartial(...siteItems);
+
+    const localSitePath = await initLocalSiteWithConfig(
+      getConfig(expectedLocalSite)
+    );
+    const server = await localServer.startInCloneMode(localSitePath);
+    await expect(loadEditor(server.port, editorSite)).rejects.toThrow();
+    process.env.NODE_ENV = "test";
   });
 });
