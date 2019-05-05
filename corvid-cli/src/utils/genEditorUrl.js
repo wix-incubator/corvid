cc5c62bd92d7522e75dda043ae3f311d442c9d11
tombenezra <tombe@wix.com>
2019-05-05 15:04:19 +0300

    #WCD-8496|propagate skip encoding flag from  env variable

--- corvid-cli/src/utils/genEditorUrl.js
+++ corvid-cli/src/utils/genEditorUrl.js
@@ -7,11 +7,15 @@ function genEditorUrl(
   builderEnvironment
 ) {
   const extraParams = process.env.QUERY ? `&${process.env.QUERY}` : "";
+  const skipEncodingParam = process.env.SKIP_LOCAL_MODE_ENCODING
+    ? "&shouldSkipLocalModeEncoding=true"
+    : "";
+
   return `${
     useSsl ? "https" : "http"
   }://${baseDomain}/editor/${metasiteId}?petri_ovr=specs.ExcludeSiteFromSsr=true&corvidSessionId=${
     process.env.CORVID_SESSION_ID
-  }&localServerPort=${localServerPort}&isHeadless=${isHeadless}&builderEnvironment=${builderEnvironment}${extraParams}`;
+  }${skipEncodingParam}&localServerPort=${localServerPort}&isHeadless=${isHeadless}&builderEnvironment=${builderEnvironment}${extraParams}`;
 }
 
 module.exports = genEditorUrl;
