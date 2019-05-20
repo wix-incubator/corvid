7a16c2dabb062aee757b16ca1d0105184d83ac01
Alvaro Morais <alvarom@wix.com>
2019-05-20 14:59:17 +0100

    corvid-local-server|Fixed originsMiddleware to allow testing

--- packages/corvid-local-server/src/server/originsMiddleWare.js
+++ packages/corvid-local-server/src/server/originsMiddleWare.js
@@ -3,12 +3,9 @@ function originsMiddleware(allowedDomains) {
   return (socket, next) => {
     const origin = socket.handshake.headers.origin || "";
     let hostname = origin.replace(/^https?:\/\//, "");
-    const isNodeEnvTest = process.env.NODE_ENV === "test";
-    if (
-      !isNodeEnvTest &&
-      allowedDomains &&
-      !allowedDomains.includes(hostname)
-    ) {
+    const isTesting =
+      process.env.NODE_ENV === "test" && hostname === "localhost:3000";
+    if (!isTesting && allowedDomains && !allowedDomains.includes(hostname)) {
       logger.warn(`refused origin [${origin}]`);
       return next(new Error("origin not allowed"));
     }
