79a7824beba6d7d34787f26686156c7d7c094298
Kosta Gershberg <konstanting@wix.com>
2019-05-29 11:13:38 +0300

    #WCD-8628|fixing node version check

--- packages/corvid-cli/src/cli.js
+++ packages/corvid-cli/src/cli.js
@@ -1,6 +1,5 @@
 const chalk = require("chalk");
 const updateNotifier = require("update-notifier");
-const version = require("./version");
 const getMessage = require("./messages");
 const packageJson = require("../package.json");
 
@@ -11,16 +10,9 @@ console.log(chalk.yellow(getMessage("Cli_Description_Yellow")));
 // eslint-disable-next-line no-console
 console.log(getMessage("Cli_Description"));
 
-if (version.check()) {
-  require("yargs")
-    .usage("Usage: $0 <command> [options]")
-    .commandDir("commands")
-    .help("help")
-    .strict()
-    .demandCommand().argv;
-} else {
-  // eslint-disable-next-line no-console
-  console.log(
-    chalk.red(getMessage("Cli_Unsupported_Node_Version"), version.required)
-  );
-}
+require("yargs")
+  .usage("Usage: $0 <command> [options]")
+  .commandDir("commands")
+  .help("help")
+  .strict()
+  .demandCommand().argv;
