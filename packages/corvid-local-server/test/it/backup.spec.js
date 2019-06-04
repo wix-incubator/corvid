ec9b49a345c8d0255c22a3f523da8a280e7cabb8
Kosta Gershberg <konstanting@wix.com>
2019-06-04 16:58:40 +0300

    debug

--- packages/corvid-local-server/test/it/backup.spec.js
+++ packages/corvid-local-server/test/it/backup.spec.js
@@ -166,21 +166,16 @@ describe("Backup", () => {
         pageFilePath,
         pageFileContent
       );
-      console.log("before eventually");
       await eventually(
         async () => {
-          console.log("inside eventually start");
           expect(onCodeChange).toHaveBeenCalledWith(watcherPayload);
           expect(onCodeChange).toHaveBeenCalledTimes(1);
           expect(onDocumentChange).toHaveBeenCalledTimes(1);
-          console.log("inside eventually end");
         },
         { timeout: 5000 }
       );
-      console.log("after eventually");
       unsubscribeFromCodeChange();
       unsubscribeFromDocumentChange();
-      console.log("after unsubscribe");
       done();
     }
   });
