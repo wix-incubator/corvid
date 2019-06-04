999f4543c5c9acf86b1dd8aa7b023638384e9f41
Kosta Gershberg <konstanting@wix.com>
2019-06-04 10:37:37 +0300

    debug

--- packages/corvid-local-server/test/it/backup.spec.js
+++ packages/corvid-local-server/test/it/backup.spec.js
@@ -142,7 +142,6 @@ describe("Backup", () => {
     try {
       await editor.save();
     } catch (e) {
-      await new Promise(res => setTimeout(res, 1000));
       const code = sc.backendCode();
       let filePath = localSiteBuilder.getLocalFilePath(code);
       let fileContent = localSiteBuilder.getLocalFileContent(code);
