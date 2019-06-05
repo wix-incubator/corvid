8b7cf621a05e511986549b4203c7b9ccb23b2c59
Dor Itzhaki <dori@wix.com>
2019-06-05 11:55:43 +0300

    ci(windows): enable windows build

---
+++ scripts/ci-windows-pre-install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+export NODEPATH=$(where.exe node.exe)
+export PROJECTDIR=$(pwd)
+export YARNCACHE=$(yarn cache dir)
+export TEMPDIR=$LOCALAPPDATA\\Temp
+
+powershell Add-MpPreference -ExclusionProcess ${NODEPATH}
+powershell Add-MpPreference -ExclusionPath ${YARNCACHE}
+powershell Add-MpPreference -ExclusionPath ${PROJECTDIR}
+powershell Add-MpPreference -ExclusionPath ${TEMPDIR}
+
+echo "DisableArchiveScanning..."
+powershell Start-Process -PassThru -Wait PowerShell -ArgumentList "'-Command Set-MpPreference -DisableArchiveScanning \$true'"
+
+echo "DisableBehaviorMonitoring..."
+powershell Start-Process -PassThru -Wait PowerShell -ArgumentList "'-Command Set-MpPreference -DisableBehaviorMonitoring \$true'"
+
+echo "DisableRealtimeMonitoring..."
+powershell Start-Process -PassThru -Wait PowerShell -ArgumentList "'-Command Set-MpPreference -DisableRealtimeMonitoring \$true'"
