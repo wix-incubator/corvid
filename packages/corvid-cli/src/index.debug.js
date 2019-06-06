aaf617fa6f5afab100488a2ec8144d7383bc498b
kostagersh <44746154+kostagersh@users.noreply.github.com>
2019-06-06 07:20:36 -0400

    #WCD-8628|fixing node version check (#40)

--- packages/corvid-cli/src/index.debug.js
+++ packages/corvid-cli/src/index.debug.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
+
+require("./ensureNodeVersion")();
+
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 process.env.LOG_LEVEL = process.env.LOG_LEVEL || "debug";
 process.env.CORVID_CLI_DISABLE_HEADLESS = true;
