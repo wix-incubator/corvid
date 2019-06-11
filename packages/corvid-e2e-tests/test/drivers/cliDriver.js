17ac87ca7beaaa818fbbf93450a8adc15aef88b7
tombenezra <tombe@wix.com>
2019-06-11 13:37:54 +0300

    output cli commands on e2e test

--- packages/corvid-e2e-tests/test/drivers/cliDriver.js
+++ packages/corvid-e2e-tests/test/drivers/cliDriver.js
@@ -2,11 +2,14 @@ const execa = require("execa");
 
 module.exports = () => {
   const clone = async ({ editorUrl }) =>
-    await execa.shellSync(`npx corvid clone ${editorUrl}`);
+    await execa.shellSync(`npx corvid clone ${editorUrl}`, {
+      stdio: "inherit"
+    });
 
   const openEditor = ({ remoteDebuggingPort }) =>
     execa.shell(
-      `npx corvid open-editor --remote-debugging-port=${remoteDebuggingPort}`
+      `npx corvid open-editor --remote-debugging-port=${remoteDebuggingPort}`,
+      { stdio: "inherit" }
     ).pid;
 
   return {
