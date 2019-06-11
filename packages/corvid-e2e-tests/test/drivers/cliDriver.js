e434091659f3c65bfe88af2df0413ca03ded957e
tombenezra <tombe@wix.com>
2019-06-11 16:14:55 +0300

    scope e2e

--- packages/corvid-e2e-tests/test/drivers/cliDriver.js
+++ packages/corvid-e2e-tests/test/drivers/cliDriver.js
@@ -2,14 +2,11 @@ const execa = require("execa");
 
 module.exports = () => {
   const clone = async ({ editorUrl }) =>
-    await execa.shellSync(`npx corvid clone ${editorUrl}`, {
-      stdio: "inherit"
-    });
+    await execa.shellSync(`npx corvid clone ${editorUrl}`);
 
   const openEditor = ({ remoteDebuggingPort }) =>
     execa.shell(
-      `npx corvid open-editor --remote-debugging-port=${remoteDebuggingPort}`,
-      { stdio: "inherit" }
+      `npx corvid open-editor --remote-debugging-port=${remoteDebuggingPort}`
     ).pid;
 
   return {
