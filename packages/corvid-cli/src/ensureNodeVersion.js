bca656ba62d885d85028d31c9de60f4a1d87534d
Dor Itzhaki <dori@wix.com>
2019-06-17 13:28:06 +0300

    style: lint fix

--- packages/corvid-cli/src/ensureNodeVersion.js
+++ packages/corvid-cli/src/ensureNodeVersion.js
@@ -2,17 +2,19 @@
 
 const minNodeVersion = 10;
 
-const checkVersion = function () {
+const checkVersion = function() {
   const nodeVersion = process.versions.node;
   const major = nodeVersion.split(".")[0];
 
   return major >= minNodeVersion;
 };
 
-module.exports = function () {
+module.exports = function() {
   if (!checkVersion()) {
     // eslint-disable-next-line no-console
-    console.error(`Unsupported Node.js version, please use version ${minNodeVersion} or higher`);
+    console.error(
+      `Unsupported Node.js version, please use version ${minNodeVersion} or higher`
+    );
     process.exit(1);
   }
 };
