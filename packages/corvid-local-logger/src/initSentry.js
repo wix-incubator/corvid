6b75202fad98bc3feeeaa145983ef537811655d1
kostagersh <44746154+kostagersh@users.noreply.github.com>
2019-07-08 05:24:33 -0400

    Adding data to sentry (#84)
    * feat(sentry): add more data to sentry
    
    Closes #WCD-8744
    
    feat(sentry): adding metasite id to sentry
    
    feat(sentry): adding command to sentry
    
    Closes #WCD-8746
    
    feat(sentry): adding santa versions to sentry
    
    adding santa and editor versions to sentry
    
    Closes #WCD-8743
    
    * fixup! feat(sentry): add more data to sentry

--- packages/corvid-local-logger/src/initSentry.js
+++ packages/corvid-local-logger/src/initSentry.js
@@ -1,14 +1,32 @@
 const os = require("os");
+const merge_ = require("lodash/merge");
 
-const initSentry = (defaultMetadata = {}) => {
+const initSentry = (defaultMetadata = {}, getSessionData) => {
   const Sentry = require("@sentry/node");
+
+  const prepareSessionData = () => {
+    const {
+      userId,
+      metasiteId,
+      command,
+      editorVersion,
+      santaVersion
+    } = getSessionData();
+
+    return {
+      user: { id: userId },
+      tags: { command },
+      extra: { metasiteId, editorVersion, santaVersion }
+    };
+  };
 
   Sentry.init({
     dsn: "https://421920d4abe64d87bea03ac821e25ed6@sentry.io/1427669",
     defaultIntegrations: [],
     release: defaultMetadata.release,
     environment: process.env.NODE_ENV,
-    enabled: !["test", "development"].includes(process.env.NODE_ENV)
+    enabled: !["test", "development"].includes(process.env.NODE_ENV),
+    beforeSend: event => merge_(prepareSessionData(), event)
   });
 
   Sentry.configureScope(scope => {
