e030b3c8031724a6ed33a4091e3dcbe1ae33e986
Dor Itzhaki <dori@wix.com>
2019-07-17 14:41:15 +0300

    refactor(tests): move the sentry testkit to corvid-local-test-utils

--- packages/corvid-local-test-utils/src/index.js
+++ packages/corvid-local-test-utils/src/index.js
@@ -2,10 +2,12 @@ const siteCreators = require("./siteCreators");
 const socketClient = require("./socketClient");
 const withClose = require("./withClose");
 const initTempDir = require("./initTempDir");
+const sentryTestkit = require("./sentry/sentryTestkit");
 
 module.exports = {
   siteCreators,
   socketClient,
   withClose,
-  initTempDir
+  initTempDir,
+  sentryTestkit
 };
---
+++ packages/corvid-local-test-utils/src/sentry/sentryDSN.js
@@ -0,0 +1,1 @@
+module.exports = "https://421920d4abe64d87bea03ac821e25ed6@sentry.io/1427669";
