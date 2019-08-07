c0050859997d4821df16d66c55b2815970f16bad
Kobi Nemni <kobin@wix.com>
2019-08-07 12:02:10 +0300

    test(corvid-e2e-tests): add login/logout tests and code reorder

--- packages/corvid-e2e-tests/test/drivers/cliDriver.js
+++ packages/corvid-e2e-tests/test/drivers/cliDriver.js
@@ -1,9 +1,10 @@
 const execa = require("execa");
 const which = require("npm-which")(__dirname);
-
+const connectToLocalEditor = require("./connectToLocalEditor");
+const { findFreePort, getCorvidTestUser } = require("./utils");
 const CORVID_BIN_PATH = which.sync("corvid");
 
-module.exports = ({ cwd }) => {
+const createCommandsDrvier = ({ cwd }) => {
   const parseCommandArgs = ({ remoteDebuggingPort } = {}) =>
     remoteDebuggingPort ? `--remote-debugging-port=${remoteDebuggingPort}` : "";
 
@@ -31,5 +32,50 @@ module.exports = ({ cwd }) => {
     logout,
     clone,
     openEditor
+  };
+};
+
+module.exports = (...args) => {
+  const commands = createCommandsDrvier(args);
+
+  const isLoggedIn = async () => {
+    const loginDebugPort = await findFreePort();
+    const loginCommand = commands.login({
+      remoteDebuggingPort: loginDebugPort
+    });
+    const loginEditorDriver = await connectToLocalEditor(loginDebugPort);
+    console.log("login connected"); //eslint-disable-line
+
+    try {
+      await loginEditorDriver.waitForLoginForm();
+      await loginEditorDriver.close();
+      return false;
+    } catch (e) {
+      await loginCommand;
+      return true;
+    }
+  };
+
+  const performLogout = async () => await commands.logout();
+
+  const performLogin = async () => {
+    const loginDebugPort = await findFreePort();
+    const loginCommand = commands.login({
+      remoteDebuggingPort: loginDebugPort
+    });
+
+    const loginEditorDriver = await connectToLocalEditor(loginDebugPort);
+    console.log("login connected"); //eslint-disable-line
+
+    await loginEditorDriver.waitForLoginForm();
+    await loginEditorDriver.login(getCorvidTestUser());
+    await loginCommand;
+  };
+
+  return {
+    commands,
+    performLogout,
+    performLogin,
+    isLoggedIn
   };
 };
