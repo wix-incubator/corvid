10ec3a4da5870435273ededaa675b19fdabd3572
Kobi Nemni <kobin@wix.com>
2019-08-07 12:02:10 +0300

    test(corvid-e2e-tests): add login/logout tests and code reorder

---
+++ packages/corvid-e2e-tests/test/drivers/connectToLocalEditor.js
@@ -0,0 +1,23 @@
+const puppeteer = require("puppeteer-core");
+const eventually = require("wix-eventually");
+const localEditorDriverCreator = require("./localEditorDriver");
+
+module.exports = async port => {
+  const browser = await eventually(
+    async () =>
+      await puppeteer.connect({
+        browserURL: `http://localhost:${port}`,
+        defaultViewport: { width: 1280, height: 960 }
+      }),
+    { timeout: 20000 }
+  );
+  const localEditorDriver = eventually(
+    async () => {
+      const [page] = await browser.pages();
+      expect(page).toBeDefined();
+      return localEditorDriverCreator(page);
+    },
+    { timeout: 20000 }
+  );
+  return localEditorDriver;
+};
