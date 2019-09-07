1a88e7f6f45ade8b53273b85ced2afa6b60dc414
Karin Agan <karinag@wix.com>
2019-09-07 17:55:42 +0300

    feat(corvid-types): add selector by type support

--- packages/corvid-types/types/pages/$w.d.ts
+++ packages/corvid-types/types/pages/$w.d.ts
@@ -1,9 +1,56 @@
-type ComponentSelectorByType = {
-  "Button": $w.Button;
-  "Image": $w.Image;
+type TypeNameToSdkType = {
+  "AccountNavBar": $w.AccountNavBar;
   "Anchor": $w.Anchor;
+  "AudioPlayer": $w.AudioPlayer;
+  "Box": $w.Box;
+  "Button": $w.Button;
+  "Checkbox": $w.Checkbox;
+  "CheckboxGroup": $w.CheckboxGroup;
+  "Column": $w.Column;
+  "ColumnStrip": $w.ColumnStrip;
+  "Container": $w.Container;
   "DatePicker": $w.DatePicker;
+  "Document": $w.Document;
+  "Dropdown": $w.Dropdown;
+  "Footer": $w.Footer;
+  "Gallery": $w.Gallery;
+  "GoogleMap": $w.GoogleMap;
+  "Header": $w.Header;
+  "HtmlComponent": $w.HtmlComponent;
+  "IFrame": $w.IFrame;
+  "Image": $w.Image;
+  "MediaBox": $w.MediaBox;
+  "MenuContainer": $w.MenuContainer;
+  "Page": $w.Page;
+  "Pagination": $w.Pagination;
+  "ProgressBar": $w.ProgressBar;
+  "QuickActionBar": $w.QuickActionBar;
+  "RadioButtonGroup": $w.RadioButtonGroup;
+  "RatingsDisplay": $w.RatingsDisplay;
+  "RatingsInput": $w.RatingsInput;
+  "Repeater": $w.Repeater;
+  "RichTextBox": $w.RichTextBox;
+  "Slide": $w.Slide;
+  "Slider": $w.Slider;
+  "Slideshow": $w.Slideshow;
+  "Switch": $w.Switch;
+  "Table": $w.Table;
+  "Text": $w.Text;
+  "TextBox": $w.TextBox;
+  "TextInput": $w.TextInput;
+  "TimePicker": $w.TimePicker;
+  "UploadButton": $w.UploadButton;
+  "VectorImage": $w.VectorImage;
+  "VerticalMenu": $w.VerticalMenu;
+  "Video": $w.Video;
+  "VideoPlayer": $w.VideoPlayer;
 }
+
+type IntersectionArrayAndBase<T> = {
+  [P in keyof T]: P extends "Document" ? T[P] : T[P] & [T[P]];
+}
+
+type ComponentSelectorByType = IntersectionArrayAndBase<TypeNameToSdkType>
 
 declare function $w<T extends keyof PageElementsMap>(selector: T): PageElementsMap[T]
 declare function $w<T extends keyof ComponentSelectorByType>(selector: T): ComponentSelectorByType[T]
