a6936bad326cf757e9b0b179b4ad85e7dcb0ce9d
tome <tome@wix.com>
2019-10-16 13:22:06 +0300

    refactor(corvid-cli): separate part that runs in electron - logout

--- packages/corvid-cli/src/commands/logout.js
+++ packages/corvid-cli/src/commands/logout.js
@@ -1,27 +1,9 @@
-/* eslint-disable no-console */
+const path = require("path");
 const chalk = require("chalk");
-const { app, BrowserWindow } = require("electron");
 const { launch } = require("../utils/electron");
 const createSpinner = require("../utils/spinner");
 const getMessage = require("../messages");
 const commandWithDefaults = require("../utils/commandWithDefaults");
-
-app &&
-  app.on("ready", async () => {
-    const win = new BrowserWindow({
-      width: 1280,
-      height: 960,
-      show: false,
-      webPreferences: { nodeIntegration: false }
-    });
-    await new Promise(resolve => {
-      win.webContents.session.clearStorageData(() => {
-        resolve();
-      });
-    });
-
-    win.close();
-  });
 
 module.exports = commandWithDefaults({
   command: "logout",
@@ -30,7 +12,7 @@ module.exports = commandWithDefaults({
     const spinner = createSpinner();
     spinner.start(chalk.grey(getMessage("Logout_Command_Clearing")));
 
-    return launch(__filename).then(() => {
+    return launch(path.join(__dirname, "../electron/logout")).then(() => {
       spinner.succeed(chalk.grey(getMessage("Logout_Command_Cleared")));
     });
   }
