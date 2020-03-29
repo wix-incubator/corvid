716851a3e52e39542f620b12d7b4b317ed39ed5f
Dor Itzhaki <dori@wix.com>
2020-03-29 17:53:59 +0300

    test(e2e): simplify login page driver

--- packages/corvid-e2e-tests/test/drivers/localEditorDriver.js
+++ packages/corvid-e2e-tests/test/drivers/localEditorDriver.js
@@ -35,17 +35,13 @@ module.exports = page => {
   };
 
   const login = async ({ username, password }) => {
-    const emaiInput = await page.$("#input_0");
+    const emaiInput = await page.$('input[name="email"]');
     await emaiInput.type(username);
-    const isNewLoginPage = !(await page.$("#input_1"));
-    if (isNewLoginPage) {
-      const continueButton = await page.$(".login-btn");
-      await continueButton.click();
-    }
-    const passwordInput = await page.waitForSelector("#input_1");
+
+    const passwordInput = await page.waitForSelector('input[name="password"]');
     await passwordInput.type(password);
-    const loginButton = await page.$(".login-btn");
-    await loginButton.click();
+
+    await passwordInput.press("Enter");
   };
 
   return {
